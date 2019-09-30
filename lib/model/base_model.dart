995f4f80c8fd8bf78b8ab8f97c48a3e5df008de6
Roberto <robertocunha@poli.ufrj.br>
2019-09-30 17:32:27 -0300

    skeleton of same methods in base_model class

---
+++ lib/model/base_model.dart
@@ -0,0 +1,22 @@
+class BaseModel {
+    /// Create a instance of a class
+    create(){}
+
+    /// Find a resource from a database via api call
+    find(id){}
+
+    ///Retrive all data from a database via api call
+    all(){}
+
+    ///save a resource from database via api call
+    save(){}
+
+    ///Update a resource from database via api call
+    update(id){}
+
+    ///Destroy a resource from database via api call
+    destroy(id){}
+
+    ///Count the number of resources retrieves form a database via api call
+    count(){}
+}
